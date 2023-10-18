00a9576b0e401ba50ad9d11378fc227a14de83c5
darkun7 <hartawanbahari@gmail.com>
2023-10-18 11:47:37 +0700

    add export with custom item type

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import subprocess
 JPEXS_PATH = "E:\\Game\!Tool\\FFDEC\\ffdec.jar"
 OUTPUT_PATH = "E:\Apps\swf2image\output"
 INPUT_PATH = "C:\\Users\\Hartawan\\AppData\\Roaming\\"
-
+#use this as references : https://github.com/jindrapetrik/jpexs-decompiler/wiki/Commandline-arguments
+ITEM_TYPE = "fla" #shape for SVG 
 
 root = os.getcwd()
-dirName = ['items']
+dirName = ['skills']
 for type in dirName:
     os.chdir(INPUT_PATH+"\\"+type)
     targetFiles = [file for file in os.listdir() if file.endswith(('swf'))]
@@ -15,5 +16,5 @@ for type in dirName:
         output_dir = OUTPUT_PATH+'\\'+type+'\\'+fname.replace('.swf','')
         if not (os.path.exists(output_dir)):
             os.makedirs(output_dir)
-        command = 'java -jar "'+JPEXS_PATH+'" -export shape "'+output_dir+'" "'+INPUT_PATH+'\\'+type+'\\'+fname+'"'
+        command = 'java -jar "'+JPEXS_PATH+'" -export '+ITEM_TYPE+' "'+output_dir+'" "'+INPUT_PATH+'\\'+type+'\\'+fname+'"'
         subprocess.run(command)
